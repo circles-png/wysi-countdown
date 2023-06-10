dcea6b1f544b35f900b9ad292975a82ade80bc03
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-06-10 23:40:08 +1000

    update styles and app

--- src/App.tsx
+++ src/App.tsx
@@ -29,25 +29,27 @@ const App = () => {
     return () => clearInterval(timer)
   }, [])
   return <>
-    <div className='h-full bg-slate-900 text-slate-200 text-xl text-center flex flex-col'>
-      <div className='h-full grid place-content-center'>
-        <div className='flex flex-col gap-8'>
-          <h1>wysi countdown</h1>
-          <span className='font-bold flex flex-col text-4xl sm:text-7xl'>
-            {time.split(', ').map((section, index) => <span className='after:content-[","]' key={index}>{section}</span>)}
-          </span>
-          <span>to the next <span className='font-medium'>7:27 pm</span></span>
+    <div className='h-full bg-grid text-slate-200 text-xl text-center'>
+      <div className='flex flex-col h-full bg-fade'>
+        <div className='h-full grid place-content-center'>
+          <div className='flex flex-col gap-8'>
+            <h1>wysi countdown</h1>
+            <span className='font-bold flex flex-col text-4xl sm:text-7xl'>
+              {time.split(', ').map((section, index) => <span className='after:content-[","]' key={index}>{section}</span>)}
+            </span>
+            <span>to the next <span className='font-medium'>7:27 pm</span></span>
+          </div>
         </div>
+        <footer className='text-slate-400 border-t border-slate-500 p-4 grow-0 shrink backdrop-blur-sm'>
+            by Matthew Li, <a
+            href='https://github.com/circles-png'
+            className='underline decoration-2 text-slate-300'
+            target='#blank'
+          >
+              @circles-png
+          </a>
+        </footer>
       </div>
-      <footer className='text-slate-400 border-t border-slate-500 p-4 grow-0 shrink'>
-        by Matthew Li, <a
-          href='https://github.com/circles-png'
-          className='underline decoration-2 text-slate-300'
-          target='#blank'
-        >
-          @circles-png
-        </a>
-      </footer>
     </div>
   </>
 }
